3202d8f69e5da4f93579afa6b65917f26e39cb87
RAINAVINAYAK16 <vraina03@gmail.com>
2025-02-08 12:25:45 +0530

    chore : edited schema of db

--- backend/model.js
+++ backend/model.js
@@ -1,33 +1,77 @@
 const mongoose = require('mongoose');
 
 const UsersSchema = new mongoose.Schema({
-    pH_level: {
+    timestamp: {
+        type: Date,
+        default: Date.now
+    },
+    record_number: {
         type: Number,
-        required: true
+        required: false
     },
     avgwaterspeed: {
         type: Number,
-        required: true
+        required: false
     },
-    salinity: {
+    avgwaterdirection: {
         type: Number,
-        required: true
-    },
-    turbidity: {
-        type: Number,
-        required: true
-    },
-    spec_conductance: {
-        type: Number,
-        required: true
-    },
-    spo2: {
-        type: Number,
-        required: true
+        required: false
     },
     chlorophyll: {
         type: Number,
-        required: true
+        required: false
+    },
+    chlorophyll_quality: {
+        type: Number,
+        required: false
+    },
+    temperature: {
+        type: Number,
+        required: false
+    },
+    temperature_quality: {
+        type: Number,
+        required: false
+    },
+    dissolved_oxygen: {
+        type: Number,
+        required: false
+    },
+    dissolved_oxygen_quality: {
+        type: Number,
+        required: false
+    },
+    pH_level: {
+        type: Number,
+        required: false
+    },
+    pH_quality: {
+        type: Number,
+        required: false
+    },
+    salinity: {
+        type: Number,
+        required: false
+    },
+    salinity_quality: {
+        type: Number,
+        required: false
+    },
+    spec_conductance: {
+        type: Number,
+        required: false
+    },
+    spec_conductance_quality: {
+        type: Number,
+        required: false
+    },
+    turbidity: {
+        type: Number,
+        required: false
+    },
+    turbidity_quality: {
+        type: Number,
+        required: false
     },
 });
 
