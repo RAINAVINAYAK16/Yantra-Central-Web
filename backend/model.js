40b3dce5078a5a9f84a1cdb3ffd112572d758d80
RAINAVINAYAK16 <vraina03@gmail.com>
2025-02-09 00:13:33 +0530

    chore : modified schema

--- backend/model.js
+++ backend/model.js
@@ -1,27 +1,7 @@
 const mongoose = require('mongoose');
 
 const UsersSchema = new mongoose.Schema({
-    timestamp: {
-        type: Date,
-        default: Date.now
-    },
-    record_number: {
-        type: Number,
-        required: false
-    },
     avgwaterspeed: {
-        type: Number,
-        required: false
-    },
-    avgwaterdirection: {
-        type: Number,
-        required: false
-    },
-    chlorophyll: {
-        type: Number,
-        required: false
-    },
-    chlorophyll_quality: {
         type: Number,
         required: false
     },
@@ -29,15 +9,7 @@ const UsersSchema = new mongoose.Schema({
         type: Number,
         required: false
     },
-    temperature_quality: {
-        type: Number,
-        required: false
-    },
     dissolved_oxygen: {
-        type: Number,
-        required: false
-    },
-    dissolved_oxygen_quality: {
         type: Number,
         required: false
     },
@@ -45,31 +17,11 @@ const UsersSchema = new mongoose.Schema({
         type: Number,
         required: false
     },
-    pH_quality: {
-        type: Number,
-        required: false
-    },
     salinity: {
         type: Number,
         required: false
     },
-    salinity_quality: {
-        type: Number,
-        required: false
-    },
-    spec_conductance: {
-        type: Number,
-        required: false
-    },
-    spec_conductance_quality: {
-        type: Number,
-        required: false
-    },
     turbidity: {
-        type: Number,
-        required: false
-    },
-    turbidity_quality: {
         type: Number,
         required: false
     },
